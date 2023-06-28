6d5b5e2b7cc6e44e78a10aeacd00d5cc6ad3a566
georgijbudik <georgijbudik@gmail.com>
2023-06-28 13:33:42 +0200

    finish hw6

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,8 @@
 const itemEl = document.querySelectorAll(".item");
-const categoryEl = document.querySelector("#categories");
 console.log(`Number of categories : `, itemEl.length);
 itemEl.forEach((item) => {
   const titleEl = item.firstElementChild.textContent;
-  const countEl = item.lastElementChild.querySelectorAll("li");
+  const countEl = item.lastElementChild.children;
   console.log(`Category: ${titleEl} 
 Elements: ${countEl.length}`);
 });
--- js/task-10.js
+++ js/task-10.js
@@ -12,17 +12,20 @@ const boxesDiv = document.getElementById("boxes");
 
 createBtn.addEventListener("click", createBoxes);
 destroyBtn.addEventListener("click", destroyBoxes);
+const min = Number(input.min);
+const max = Number(input.max);
+const step = Number(input.step);
 
 function createBoxes() {
   const amount = Number(input.value);
-  if (amount < 1 || amount > 100) {
+  if (amount > max || amount < min) {
     alert("Please enter a number between 1 and 100");
     return;
   }
   const boxes = [];
   let boxSize = 30;
 
-  for (let i = 0; i < amount; i += 1) {
+  for (let i = min; i <= amount; i += step) {
     const box = document.createElement("div");
     box.style.width = boxSize + "px";
     box.style.height = boxSize + "px";
